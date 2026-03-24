c113cde8e5cdce0c1aceb937a2b98d2de6d6159e
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-03-24 18:47:13 -0300

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
 from dotenv import load_dotenv
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
-from telegram_bot.control import start_command, botones_handler, registrar
 import os
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
+from fron import start_command, botones_handler, registrar
 
+# ---------------- CONFIG ----------------
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 if not TOKEN:
     raise Exception("❌ Debes definir tu TOKEN en las variables de entorno")
 
+# ---------------- INICIALIZAR BOT ----------------
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start_command))
 app.add_handler(CallbackQueryHandler(botones_handler))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, registrar))
+
+print("💀 KHASAM BOT ACTIVADO")
 app.run_polling()
